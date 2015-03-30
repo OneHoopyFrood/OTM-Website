6d756802220101225b9315a94f056f2dd90e0534
Cole Panike <colepanike@gmail.com>
2015-03-30 15:17:50 -0600

    Seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,29 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create!(fname: "Cole",
+             lname: "Panike",
+             email: "colepanike@gmail.com"
+             password: "superadmin",
+             password_confirmation: "superadmin",
+             admin: true )
+
+12.times do |n|
+  fname  = Faker::Name.first_name
+  lname = Faker::Name.last_name
+  email = "example-#{n+1}@example.com"
+  password = "password"
+  User.create!(fname:  fname,
+               lname: lname,
+               email: email,
+               password:              password,
+               password_confirmation: password)
+end
+
+25.times do |n|
+  title = Faker::App.Name
+  body = Faker::Lorem.paragraph(4)
+  user = User.order("RANDOM()").first
+
+  Post.create!(
+    title: title,
+    body: body,
+    user: user )
+end
